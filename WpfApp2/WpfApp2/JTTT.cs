bd93cf75b95114cfd000c20aa3039ef47a304343
RaV <a.rivendare@o2.pl>
2018-03-13 23:55:50 +0100

    Obsługa komunikatów, obsługa błędów pustego pola, kolor tła

---
+++ WpfApp2/WpfApp2/JTTT.cs
@@ -0,0 +1,179 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace WpfApp2
+{
+    public class JTTT
+    {
+        public string url = "";
+        public string text = "";
+        public string mail = "";
+        public string filename = "";
+        public string errorStr = "";
+
+        public string FindImage(WebClient site)
+        {
+            string linkToImage = "";
+            bool imageExist = false;
+            if (checkSite(this.url) == true)
+            {
+                string url = site.DownloadString(this.url);
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(url);
+                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    if (link.InnerHtml.Contains(this.text) && link.InnerHtml.Contains("http"))
+                    {
+                        imageExist = true;
+                        int indexOfImage = link.InnerHtml.IndexOf("src=\"") + "src=\"".Length;
+                        char tmp = '"';
+                        while (link.InnerHtml[indexOfImage] != tmp)
+                        {
+                            linkToImage += link.InnerHtml[indexOfImage].ToString();
+                            indexOfImage++;
+                        }
+                    }
+                    if (imageExist) return linkToImage;
+                    else errorStr = "image";
+                }
+                return linkToImage;
+            }
+            else
+            {
+                errorStr = "address";
+                return linkToImage;
+            }
+        }
+
+        public bool checkSite(string url)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "HEAD";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool checkEmail()
+        {
+            try
+            {
+                if (this.mail != "")
+                {
+                    MailAddress m = new MailAddress(this.mail);
+
+
+                    return true;
+                }
+                return false;
+            }
+            catch (FormatException)
+            {
+                errorStr = "mail";
+                return false;
+            }
+        }
+
+        public bool SendMail(string linkToImage)
+        {
+            var fromAddress = new MailAddress("wysylaczmemow@gmail.com", "");
+            if (this.checkEmail() == true)
+            {
+                var toAddress = new MailAddress(this.mail, "");
+                const string fromPassword = "haselkomaselko";
+                const string subject = "Wiadomość z programu JTTT";
+                string body = "Wiadomość wygenerowana automatycznie, oto link do Twojego zdjęcia: " + linkToImage;
+
+                Attachment attachment;
+                attachment = new Attachment(this.filename);
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body,
+                })
+                {
+                    message.Attachments.Add(attachment);
+                    smtp.Send(message);    
+                    System.IO.File.AppendAllText("./log.log", "Tekst: " + this.text + " URL: " + this.url + " EMAIL: " + this.mail + "\r\n");
+                }
+                errorStr = "complete";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool checkForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (client.OpenRead("http://clients3.google.com/generate_204"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                errorStr = "internet";
+                return false;
+            }
+        }
+
+        public void work()
+        {
+            WebClient site = new WebClient();
+            Uri uriResult;
+            bool result = Uri.TryCreate(this.url, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (result)
+            {
+                if (checkForInternetConnection())
+                {
+                    string linkToImage = FindImage(site);
+                    if (linkToImage == "") { }
+                    else
+                    {
+                        this.filename = @".\meme.png";
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile(linkToImage, this.filename);
+                        }
+                        SendMail(linkToImage);
+
+                    }
+                }
+            }
+            else errorStr = "address";
+        }
+       
+       
+
+    }
+
+  
+
+}
--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.ComponentModel;
-using System.Net;
-using System.Net.Mail;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
@@ -14,8 +11,7 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private BackgroundWorker m_oBackgroundWorker = null;
+        
 
         public MainWindow()
         {
@@ -29,12 +25,13 @@ namespace WpfApp2
 
         private void WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            outputBox.Text = "Pracuje, czekaj...";
-            outputBox.Background = Brushes.OrangeRed;
+            
         }
 
        public void StartWork()
         {
+            outputBox.Text = "Pracuje, czekaj...";
+            outputBox.Background = Brushes.OrangeRed;
             BackgroundWorker worker = new BackgroundWorker();
             worker.DoWork += DoWork;
             worker.RunWorkerCompleted += WorkerCompleted;
@@ -46,131 +43,53 @@ namespace WpfApp2
             StartWork();
         }
 
-        public bool checkForInternetConnection()
-        {
-            try
-            {
-                using (var client = new WebClient())
-                {
-                    using (client.OpenRead("http://clients3.google.com/generate_204"))
-                    {
-                        return true;
-                    }
-                }
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
         public void work()
         {
-            
-            SolidColorBrush brush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF283655"));
-            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => 
+            Color color = (Color)ColorConverter.ConvertFromString("#FF283655");
+            SolidColorBrush brush = new SolidColorBrush(color);
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
             {
-            WebClient site = new WebClient();
-            Uri uriResult;
-            bool result = Uri.TryCreate(urlBox.Text, UriKind.Absolute, out uriResult)
-                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-            if (result)
-            {
-                if (checkForInternetConnection())
+                JTTT newTask = new JTTT();
+                newTask.url = urlBox.Text;
+                newTask.text = textBox.Text;
+                newTask.mail = mailBox.Text;
+                if (newTask.url == "" || newTask.text == "" || newTask.mail == "")
                 {
-                    string linkToImage = "";
-                    string imageExtension = "";
-                    string url = site.DownloadString(urlBox.Text);
-                    bool isExtension = false;
-                    HtmlDocument doc = new HtmlDocument();
-                    doc.LoadHtml(url);
-                    bool imageExist = false;
-                    foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                    outputBox.Background = Brushes.Red;
+                    outputBox.Text = "Wypełnij wszystkie pola";
+                }
+                else
+                {
+                    newTask.work();
+                    switch (newTask.errorStr)
                     {
-                        if (link.InnerHtml.Contains(textBox.Text) && link.InnerHtml.Contains("http"))
-                        {
-                            imageExist = true;
-                            int indexOfImage = link.InnerHtml.IndexOf("src=\"") + "src=\"".Length;
-                            char tmp = '"';
-                            while (link.InnerHtml[indexOfImage] != tmp)
-                            {
-                                linkToImage += link.InnerHtml[indexOfImage].ToString();
-                                if (link.InnerHtml[indexOfImage].ToString() == ".") isExtension = true;
-                                if (isExtension) imageExtension += link.InnerHtml[indexOfImage].ToString();
-                                indexOfImage++;
-                            }
-                        }
-                        if (!imageExist) { outputBox.Text = "Nie znaleziono słowa na stronie"; outputBox.Background = Brushes.Red; }
-                        else
-                        {
-                            string localFilename = @".\meme.png";
-                            using (WebClient client = new WebClient())
-                            {
-                                client.DownloadFile(linkToImage, localFilename);
-                            }
-
-
-
-                            var fromAddress = new MailAddress("wysylaczmemow@gmail.com", "");
-                            if (checkEmail() == true)
-                            {
-                                var toAddress = new MailAddress(mailBox.Text, "");
-                                const string fromPassword = "haselkomaselko";
-                                const string subject = "Wiadomość z programu JTTT";
-                                string body = "Wiadomość wygenerowana automatycznie, oto link do Twojego zdjęcia: " + linkToImage;
-
-                                Attachment attachment;
-                                attachment = new Attachment(localFilename);
-
-                                var smtp = new SmtpClient
-                                {
-                                    Host = "smtp.gmail.com",
-                                    Port = 587,
-                                    EnableSsl = true,
-                                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                                    UseDefaultCredentials = false,
-                                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                                };
-                                using (var message = new MailMessage(fromAddress, toAddress)
-                                {
-                                    Subject = subject,
-                                    Body = body,
-                                })
-                                {
-                                    message.Attachments.Add(attachment);
-                                    smtp.Send(message);
-                                    outputBox.Text = "Poprawnie wysłano wiadomość";
-                                    System.IO.File.AppendAllText("./log.log", "Tekst: " + textBox.Text + " URL: " + urlBox.Text + " EMAIL: " + mailBox.Text+"\r\n");
-                                    outputBox.Background = Brushes.Green;
-                                }
-                                return;
-                            }
-                            else
-                            {
-                                outputBox.Background = Brushes.Red;
-                                outputBox.Text = "Podano nieprawidłowy adres email";
-                            }
-                        }
+                        case "image":
+                            outputBox.Background = Brushes.Red;
+                            outputBox.Text = "Nie znaleziono obrazka";
+                            break;
+                        case "internet":
+                            outputBox.Background = Brushes.Red;
+                            outputBox.Text = "Brak połączenia z internetem";
+                            break;
+                        case "mail":
+                            outputBox.Background = Brushes.Red;
+                            outputBox.Text = "Adres mail jest niepoprawny";
+                            break;
+                        case "address":
+                            outputBox.Background = Brushes.Red;
+                            outputBox.Text = "Adres URL jest niepoprawny";
+                            break;
+                        case "complete":
+                            outputBox.Background = Brushes.Green;
+                            outputBox.Text = "Pomyślnie wysłano wiadomość";
+                            break;
+                        default:
+                            outputBox.Background = brush;
+                            outputBox.Text = "Oczekiwanie na użytkownika";
+                            break;
                     }
                 }
-            }
-            else outputBox.Text = "Proszę wpisać poprawny adres strony";
-            outputBox.Background = Brushes.Red;
             }));
-        }
-
-        public bool checkEmail()
-        {
-            try
-            {
-                MailAddress m = new MailAddress(mailBox.Text);
-
-                return true;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
         }
 
 
