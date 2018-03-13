af60969b64dc441267d048f0e23bea5b5a66bc7a
RaV <a.rivendare@o2.pl>
2018-03-13 15:55:45 +0100

    asd

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp2
 {
@@ -24,20 +27,67 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private BackgroundWorker m_oBackgroundWorker = null;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            work();
+        }
+
+        private void WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            outputBox.Text = "Pracuje, czekaj...";
+            outputBox.Background = Brushes.OrangeRed;
+        }
+
+       public void StartWork()
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += DoWork;
+            worker.RunWorkerCompleted += WorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
         private void startBtn_Click(object sender, RoutedEventArgs e)
         {
+            StartWork();
+        }
+
+        public bool checkForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (client.OpenRead("http://clients3.google.com/generate_204"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void work()
+        {
+            SolidColorBrush brush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF283655"));
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => 
+            {
             WebClient site = new WebClient();
             Uri uriResult;
             bool result = Uri.TryCreate(urlBox.Text, UriKind.Absolute, out uriResult)
                 && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
             if (result)
             {
-                label1.Content = "";
                 if (checkForInternetConnection())
                 {
                     string linkToImage = "";
@@ -62,10 +112,12 @@ namespace WpfApp2
                                 indexOfImage++;
                             }
                         }
-                        if (!imageExist) label2.Content = "Takie słowo nie występuje na podanej stronie";
+                        if (!imageExist) { outputBox.Text = "Nie znaleziono słowa na stronie"; outputBox.Background = Brushes.Red; }
                         else
                         {
-                            label2.Content = "";
+                            outputBox.Text = "";
+
+                            outputBox.Background = brush;
 
                             string localFilename = @".\meme.png";
                             using (WebClient client = new WebClient())
@@ -73,55 +125,64 @@ namespace WpfApp2
                                 client.DownloadFile(linkToImage, localFilename);
                             }
 
+
+
                             var fromAddress = new MailAddress("wysylaczmemow@gmail.com", "");
-                            var toAddress = new MailAddress(mailBox.Text, "");
-                            const string fromPassword = "haselkomaselko";
-                            const string subject = "Memiczna wiadomość";
-                            string body = "Wiadomość wygenerowana automatycznie, oto link do Twojego zdjęcia: " + linkToImage;
+                            if (checkEmail() == true)
+                            {
+                                var toAddress = new MailAddress(mailBox.Text, "");
+                                const string fromPassword = "haselkomaselko";
+                                const string subject = "Wiadomość z programu JTTT";
+                                string body = "Wiadomość wygenerowana automatycznie, oto link do Twojego zdjęcia: " + linkToImage;
 
-                            Attachment attachment;
-                            attachment = new Attachment(localFilename);
+                                Attachment attachment;
+                                attachment = new Attachment(localFilename);
 
-                            var smtp = new SmtpClient
-                            {
-                                Host = "smtp.gmail.com",
-                                Port = 587,
-                                EnableSsl = true,
-                                DeliveryMethod = SmtpDeliveryMethod.Network,
-                                UseDefaultCredentials = false,
-                                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                            };
-                            using (var message = new MailMessage(fromAddress, toAddress)
-                            {
-                                Subject = subject,
-                                Body = body,
-                            })
-                            {
-                                message.Attachments.Add(attachment);
-                                smtp.Send(message);
-                                label3.Content = "Poprawnie wysłano wiadomość";
+                                var smtp = new SmtpClient
+                                {
+                                    Host = "smtp.gmail.com",
+                                    Port = 587,
+                                    EnableSsl = true,
+                                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                                    UseDefaultCredentials = false,
+                                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                                };
+                                using (var message = new MailMessage(fromAddress, toAddress)
+                                {
+                                    Subject = subject,
+                                    Body = body,
+                                })
+                                {
+                                    message.Attachments.Add(attachment);
+                                    smtp.Send(message);
+                                    outputBox.Text = "Poprawnie wysłano wiadomość";
+                                    outputBox.Background = Brushes.Green;
+                                }
+                                return;
                             }
-                            return;
+                            else
+                            {
+                                outputBox.Background = Brushes.Red;
+                                outputBox.Text = "Podano nieprawidłowy adres email";
+                            }
                         }
                     }
                 }
             }
-            else label1.Content = "Proszę wpisać poprawny adres strony";
+            else outputBox.Text = "Proszę wpisać poprawny adres strony";
+            outputBox.Background = Brushes.Red;
+            }));
         }
 
-        public bool checkForInternetConnection()
+        public bool checkEmail()
         {
             try
             {
-                using (var client = new WebClient())
-                {
-                    using (client.OpenRead("http://clients3.google.com/generate_204"))
-                    {
-                        return true;
-                    }
-                }
+                MailAddress m = new MailAddress(mailBox.Text);
+
+                return true;
             }
-            catch
+            catch (FormatException)
             {
                 return false;
             }
